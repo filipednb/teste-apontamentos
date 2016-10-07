f3d0cc8fbc1006d544c3b433f616fa3082de03d5
Filipe Tagliacozzi <filipednb@gmail.com>
2016-10-07 09:12:16 -0300

    imagens e polimento

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 	Main.init();
 	$('[data-toggle="offcanvas"]').click(function(){
-       $("#navigation").toggleClass("hidden-xs");
+       $('#navigation').toggleClass('hidden-xs');
    });
 });
 
@@ -10,30 +10,27 @@ $(document).ready(function(){
 var Main = {
 	init: function() { 
 		//definindo ações dos botões do menu
-		$('#app-main-content').load('views/apontamentos.html',function(){
+		$('#app-main-content').load('apontamentos.html',function(){
 			Apontamentos.init();
 		});	
 		$('#menu-apontamentos').click(function() {
-			$('#app-main-content').load('views/apontamentos.html',function(){
+			$('#app-main-content').load('apontamentos.html',function(){
 				Apontamentos.init();
 			});			
 		});
 		$('#menu-charts').click(function() {
-			$('#app-main-content').load('views/graficos.html',function(){
+			$('#app-main-content').load('graficos.html',function(){
 				Graficos.init();
 			});			
 		});
-		
-		//Já alimenta o WebStorage com os dados...
-		//Network.init();
 	}
 }
 
 Number.prototype.toTime = function(){
   var hrs = Math.floor(this)
   var min = Math.round(this%1*60)
-  min = min<10 ? "0"+min : min.toString();
-  return hrs+":"+min;
+  min = min<10 ? '0'+min : min.toString();
+  return hrs+':'+min;
 }
 
 
